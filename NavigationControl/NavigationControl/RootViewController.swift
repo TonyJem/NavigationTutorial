1edb005e3c28d7a46262ce249ed15e48afded04f
TonyJem <a.jemanov@gmail.com>
2021-05-14 22:56:44 +0300

    Update RootViewController.swift

--- NavigationControl/NavigationControl/RootViewController.swift
+++ NavigationControl/NavigationControl/RootViewController.swift
@@ -3,13 +3,42 @@
 import UIKit
 
 class RootViewController: UIViewController {
-
+    
+    private var current: UIViewController
+    
+    init() {
+       self.current = SplashViewController()
+       super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
+
         view.backgroundColor = .systemGray2
         print("🟢 viewDidLoad")
+        
+        addChild(current)
+        current.view.frame = view.bounds
+        view.addSubview(current.view)
+        current.didMove(toParent: self)
+    }
+    
+    func showLoginScreen() {
+        let new = UINavigationController(rootViewController: LoginViewController())
+        
+        addChild(new)
+        new.view.frame = view.bounds
+        view.addSubview(new.view)
+        new.didMove(toParent: self)
+        
+        current.willMove(toParent: nil)
+        current.view.removeFromSuperview()
+        current.removeFromParent()
+        
+        current = new
     }
 }
