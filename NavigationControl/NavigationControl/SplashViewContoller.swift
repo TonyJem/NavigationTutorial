16db7866c252b630392c09bef969320ababad1cd
TonyJem <a.jemanov@gmail.com>
2021-05-14 22:45:26 +0300

    Create SplashViewController

---
+++ NavigationControl/NavigationControl/SplashViewContoller.swift
@@ -0,0 +1,28 @@
+import  UIKit
+
+class SplashViewController: UIViewController {
+    private let activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
+    
+   override func viewDidLoad() {
+      super.viewDidLoad()
+    
+      view.backgroundColor = UIColor.white
+      view.addSubview(activityIndicator)
+      activityIndicator.frame = view.bounds
+      activityIndicator.backgroundColor = UIColor(white: 0, alpha: 0.4)
+      makeServiceCall()
+   }
+    
+    private func makeServiceCall() {
+       activityIndicator.startAnimating()
+       DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(3)) {
+          self.activityIndicator.stopAnimating()
+        
+        if UserDefaults.standard.bool(forKey: "LOGGED_IN") {
+                 // navigate to protected page
+              } else {
+                 // navigate to login screen
+              }
+       }
+    }
+}
