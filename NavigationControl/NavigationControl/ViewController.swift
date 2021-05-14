69cb1926da040927400150c83a50afcb585becee
TonyJem <a.jemanov@gmail.com>
2021-05-14 22:06:29 +0300

    Create blank project

---
+++ NavigationControl/NavigationControl/ViewController.swift
@@ -0,0 +1,10 @@
+// Based on tutorial
+// https://stasost.medium.com/ios-root-controller-navigation-3625eedbbff
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
