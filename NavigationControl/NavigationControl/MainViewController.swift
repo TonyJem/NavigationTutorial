bb57b58a6b70b37616c81adc9758f0b7c4c48af8
TonyJem <a.jemanov@gmail.com>
2021-05-14 22:49:40 +0300

    Create MainViewController

---
+++ NavigationControl/NavigationControl/MainViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class MainViewController: UIViewController {
+    
+   override func viewDidLoad() {
+      super.viewDidLoad()
+    
+      view.backgroundColor = UIColor.lightGray // to visually distinguish the protected part
+      title = "Main Screen"
+      let logoutButton = UIBarButtonItem(title: "Log Out", style: .plain, target: self, action: #selector(logout))
+      navigationItem.setLeftBarButton(logoutButton, animated: true)
+   }
+    
+   @objc
+   private func logout() {
+      // clear the user session (example only, not for the production)
+      UserDefaults.standard.set(false, forKey: "LOGGED_IN")
+      // navigate to the Main Screen
+   }
+}
