cb6b6df3a227221d99219afa01c85b9ab6ec2fcb
TonyJem <a.jemanov@gmail.com>
2021-05-14 22:46:54 +0300

    Create LoginViewController

---
+++ NavigationControl/NavigationControl/LoginViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+   override func viewDidLoad() {
+      super.viewDidLoad()
+    
+      view.backgroundColor = UIColor.white
+      title = "Login Screen"
+      let loginButton = UIBarButtonItem(title: "Log In", style: .plain, target: self, action: #selector(login))
+      navigationItem.setLeftBarButton(loginButton, animated: true)
+   }
+    
+@objc
+   private func login() {
+      // store the user session (example only, not for the production)
+      UserDefaults.standard.set(true, forKey: "LOGGED_IN")
+      // navigate to the Main Screen
+   }
+}
