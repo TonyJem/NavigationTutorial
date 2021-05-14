5a4be1ce4ffbc0b166b87f3f438c97567b7b790b
TonyJem <a.jemanov@gmail.com>
2021-05-14 23:05:49 +0300

    Update MainViewController.swift

--- NavigationControl/NavigationControl/MainViewController.swift
+++ NavigationControl/NavigationControl/MainViewController.swift
@@ -2,19 +2,19 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-   override func viewDidLoad() {
-      super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.lightGray // to visually distinguish the protected part
+        title = "Main Screen"
+        let logoutButton = UIBarButtonItem(title: "Log Out", style: .plain, target: self, action: #selector(logout))
+        navigationItem.setLeftBarButton(logoutButton, animated: true)
+    }
     
-      view.backgroundColor = UIColor.lightGray // to visually distinguish the protected part
-      title = "Main Screen"
-      let logoutButton = UIBarButtonItem(title: "Log Out", style: .plain, target: self, action: #selector(logout))
-      navigationItem.setLeftBarButton(logoutButton, animated: true)
-   }
-    
-   @objc
-   private func logout() {
-      // clear the user session (example only, not for the production)
-      UserDefaults.standard.set(false, forKey: "LOGGED_IN")
-      // navigate to the Main Screen
-   }
+    @objc
+    private func logout() {
+        // clear the user session (example only, not for the production)
+        UserDefaults.standard.set(false, forKey: "LOGGED_IN")
+        // navigate to the Main Screen
+    }
 }
