df3572135434fc95b29dde03080b5881a1c1b28c
TonyJem <a.jemanov@gmail.com>
2021-05-14 23:10:26 +0300

    Update SplashViewContoller.swift

--- NavigationControl/NavigationControl/SplashViewContoller.swift
+++ NavigationControl/NavigationControl/SplashViewContoller.swift
@@ -3,26 +3,28 @@ import  UIKit
 class SplashViewController: UIViewController {
     private let activityIndicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
     
-   override func viewDidLoad() {
-      super.viewDidLoad()
-    
-      view.backgroundColor = UIColor.white
-      view.addSubview(activityIndicator)
-      activityIndicator.frame = view.bounds
-      activityIndicator.backgroundColor = UIColor(white: 0, alpha: 0.4)
-      makeServiceCall()
-   }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
+        view.addSubview(activityIndicator)
+        activityIndicator.frame = view.bounds
+        activityIndicator.backgroundColor = UIColor(white: 0, alpha: 0.4)
+        makeServiceCall()
+    }
     
     private func makeServiceCall() {
-       activityIndicator.startAnimating()
-       DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(3)) {
-          self.activityIndicator.stopAnimating()
-        
-        if UserDefaults.standard.bool(forKey: "LOGGED_IN") {
-                 // navigate to protected page
-              } else {
-                 // navigate to login screen
-              }
-       }
+        activityIndicator.startAnimating()
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(3)) {
+            self.activityIndicator.stopAnimating()
+            
+            if UserDefaults.standard.bool(forKey: "LOGGED_IN") {
+                // navigate to protected page
+                SceneDelegate.shared.rootViewController.switchToMainScreen()
+            } else {
+                // navigate to login screen
+                SceneDelegate.shared.rootViewController.switchToLogout()
+            }
+        }
     }
 }
